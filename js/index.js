ee06d0b3d387cacd821dde230fa0f56ac86b3c27
Aaron Goodman <Amgoodman5@gmail.com>
2015-07-18 18:36:42 -0600

    <trouble with the img>

--- js/index.js
+++ js/index.js
@@ -6,30 +6,30 @@ $(document).ready(function() {
         $("button").removeClass("selected");
         $(this).addClass("selected");
 
-
+        //url
         var tnAPI = "https://s3.amazonaws.com/intern-coding-challenge/counts.json";
-        var icons = $(this).text();
-        
+        $(this).append();{
 
+        }
         function displayIcon(data) {
+
             //tn is TomNod
 
             var tnHTML = '<ul>';
-            $.each(data.tags,function(i,tags) {
+            $.each(data,function(tags) {
+                //thumbnail grid
                 tnHTML += '<li class="grid-25 tablet-grid-50">';
-                tnHTML += '<a href="' + tags.icon_url.name + '" class="image">';
-                tnHTML += '<img src="' + tags.icon_url.name + '"></a></li>';
+                tnHTML += '<a href="' + tags.icon_url + '" class="image">';
+                tnHTML += '<img src="' + tags + '"></a></li>';
             }); // end each
             tnHTML += '</ul>';
             $('#icons').html(tnHTML);
         }
-        $.getJSON(tnAPI,icons, displayIcon);
+        $.getJSON(tnAPI, displayIcon);
 
 
 
-        var jsonString='{"icon_url": "/nod/images/icons/people_drowned_60px.png","icon_url": "/nod/images/icons/svg/sport_tennisball.svg",}'
-        var icon= JSON.parse(jsonString);
-        console.log(icon.icon);
+
 
 
     }); // end click
