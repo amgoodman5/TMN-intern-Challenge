2fa94a1c83c01d477e100d0d4477bf5c8ba32a32
Aaron Goodman <Amgoodman5@gmail.com>
2015-07-20 13:58:58 -0600

    <.>

--- js/index.js
+++ js/index.js
@@ -1,37 +1,74 @@
-$(document).ready(function() {
-
-
-    $('button').click(function () {
-
-        $("button").removeClass("selected");
-        $(this).addClass("selected");
-
-        //url
-        var tnAPI = "https://s3.amazonaws.com/intern-coding-challenge/counts.json";
-        $(this).append();{
-
-        }
-        function displayIcon(data) {
-
-            //tn is TomNod
-
-            var tnHTML = '<ul>';
-            $.each(data,function(tags) {
-                //thumbnail grid
-                tnHTML += '<li class="grid-25 tablet-grid-50">';
-                tnHTML += '<a href="' + tags.icon_url + '" class="image">';
-                tnHTML += '<img src="' + tags + '"></a></li>';
-            }); // end each
-            tnHTML += '</ul>';
-            $('#icons').html(tnHTML);
-        }
-        $.getJSON(tnAPI, displayIcon);
+ $(document).ready(function(){
+     $(window).load(function(){ data(); });});
 
 
 
 
 
 
-    }); // end click
 
-}); // end ready
+ /**
+  //{
+  "name": "TomNod API",
+  "version": "0.0.1"
+
+
+"tags": [
+{
+"type_id": "118",
+"name": "Swimming Pool",
+"color": "15ff00",
+"icon_url": "/nod/images/icons/people_drowned_60px.png",
+"count": 2601
+},
+{
+"type_id": "248",
+"name": "Tennis Court",
+"color": "ff00e1",
+"icon_url": "/nod/images/icons/svg/sport_tennisball.svg",
+"count": 326
+},
+{
+"type_id": "252",
+"name": "Soccer Field",
+"color": "0099ff",
+"icon_url": "/nod/images/icons/svg/sport_soccer_ball.svg",
+"count": 728
+}
+],
+"clusters": [
+{
+"type_id": "118",
+"name": "Swimming Pool",
+"color": "15ff00",
+"icon_url": "/nod/images/icons/people_drowned_60px.png",
+"count": 652
+},
+{
+"type_id": "248",
+"name": "Tennis Court",
+"color": "ff00e1",
+"icon_url": "/nod/images/icons/svg/sport_tennisball.svg",
+"count": 31
+},
+{
+"type_id": "252",
+"name": "Soccer Field",
+"color": "0099ff",
+"icon_url": "/nod/images/icons/svg/sport_soccer_ball.svg",
+"count": 97
+}
+],
+"total_tags": 3655,
+"total_clusters": 780,
+"total_maps": 22057,
+"total_area": 3962.6420033322,
+"views": 138420,
+"taggers": 377,
+"recent_taggers": "0",
+"recent_time": 5,
+"status": 1,
+"msg": "Counted 0 taggers in last 5 minutes"
+}
+  * Created by AaronG on 7/19/2015.
+  */
